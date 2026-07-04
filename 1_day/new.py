1dce3e54c7441adddcfb120ed8013b5ea459ef79
AYUSH SHARMA <ayushbtechcse7@gmail.com>
2026-07-04 21:42:20 +0530

    new

---
+++ 1_day/new.py
@@ -0,0 +1,16 @@
+name="Anirudh"
+age=25.5
+gender='Male'
+is_student=True
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(gender))
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+
+total = num1 + num2
+print(f"Total = {total}")
