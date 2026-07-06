d5fe51d1adb580b77081f47958144cb1f06fde2f
AYUSH SHARMA <ayushbtechcse7@gmail.com>
2026-07-06 22:15:23 +0530

    Implement bubble sort for a list
    Added bubble sort implementation to sort a list.

--- linearsearrch.py
+++ linearsearrch.py
@@ -13,3 +13,18 @@ if result != -1:
   print("Found at index", result)
 else:
   print("Not found")
+
+
+mylist = [7, 3, 9, 12, 11]
+
+n = len(mylist)
+for i in range(n-1):
+  swapped = False
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+      swapped = True
+  if not swapped:
+    break
+
+print(mylist)
